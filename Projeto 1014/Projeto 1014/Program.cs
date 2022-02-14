9cb0a7a85f1c05746a5965c23754e740c2470e8c
karluze <karluzem@gmail.com>
2022-02-14 16:17:55 -0300

    Projeto 1014

---
+++ Projeto 1014/Projeto 1014/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double X, Y, resultado;
+
+            X = double.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            resultado = X / Y;
+
+            Console.WriteLine(resultado.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+
+            //Console.ReadLine();
+        }
+    }
+}
+
+
